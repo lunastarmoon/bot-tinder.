f339f98855284f9c2df2252b305b62a930585575
lunastarmoon <lilixboki@gmail.com>
2026-07-31 01:53:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import threading
+import time 
 
 import telebot
 from telebot.types import (
@@ -1845,13 +1846,23 @@ def status_bot(message):
 
 def iniciar_bot():
 
-    print("🤖 Bot iniciado!")
+    while True:
 
-    bot.infinity_polling(
-        timeout=60,
-        long_polling_timeout=60
-    )
+        try:
 
+            print("🤖 Bot iniciado!")
+
+            bot.infinity_polling(
+                timeout=60,
+                long_polling_timeout=60,
+                skip_pending=True
+            )
+
+        except Exception as erro:
+
+            print(f"Erro no bot: {erro}")
+
+            time.sleep(5)
 
 # ==========================================================
 # EXECUTAR BOT
