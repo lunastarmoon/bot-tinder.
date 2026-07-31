3679ac30a32f3538153064a3d2bad68b7708baec
lunastarmoon <lilixboki@gmail.com>
2026-07-31 00:54:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 # CONFIGURAÇÕES
 # ==========================================================
 
-TOKEN = os.getenv('8733102844:AAF0Eif5ogzAqFcna5Z3xOv57yC8F-PXx-Y')
+TOKEN = '8733102844:AAF0Eif5ogzAqFcna5Z3xOv57yC8F-PXx-Y'
 
 print("TOKEN carregado?", TOKEN is not None)
 
