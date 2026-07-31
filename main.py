0120ae2f81523d1a423182c85f15088c584787a1
lunastarmoon <lilixboki@gmail.com>
2026-07-31 00:03:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 # CONFIGURAÇÕES
 # ==========================================================
 
-TOKEN = '8733102844:AAHQ4m6OknkuFEiwv5GbLnU4oi_xiWAoQ5o'
+TOKEN = os.getenv("8733102844:AAF0Eif5ogzAqFcna5Z3xOv57yC8F-PXx-Y")
 
 bot = telebot.TeleBot(TOKEN, parse_mode="Markdown")
 
